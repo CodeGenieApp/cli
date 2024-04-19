a2598ad8f6bdc035eb37bb9447fba62c11fe44ee
Brett <brett.j.andrews@gmail.com>
2024-04-19 20:29:21 +0930

    fix: export types

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-export {run} from '@oclif/core'
+export {
+  AppDefinition,
+  Auth,
+  BaseProperty,
+  DateProperty,
+  DynamicDefault,
+  Entities,
+  Entity,
+  EntityDynamoDb,
+  EntityGSI,
+  EntityLSI,
+  EntityUi,
+  NumberProperty,
+  Properties,
+  Property,
+  PropertyUi,
+  StringProperty,
+} from './input/types.js'
+export { run } from '@oclif/core'
